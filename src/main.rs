6accf870f1120ac647ed1aa8f00f6d2f2c9d4ef1
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-01-09 13:15:50 +0700

    tests: add insta json tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufRead};
 
+#[derive(serde::Serialize, Clone)]
 struct Task {
     pub name: String,
     pub is_completed: bool,
@@ -21,16 +22,16 @@ fn main() -> io::Result<()> {
         list_tasks(&tasks);
 
         let option = readline();
-        _ = match option.as_str() {
+        match option.as_str() {
             "1" => {
                 println!("Enter new task name: ");
                 let name = readline();
-                add_task(&mut tasks, name);
+                _ = add_task(&mut tasks, name);
             },
             "2" => {
                 println!("Enter task to complete: ");
                 let level: i32 = readline().parse::<i32>().unwrap();
-                complete_task(&mut tasks, level);
+                _ = complete_task(&mut tasks, level);
             },
             _ => break,
         };
@@ -38,12 +39,13 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn add_task(tasks: &mut Vec<Task>, name: String) -> io::Result<()> {
-    tasks.push(Task {
+fn add_task(tasks: &mut Vec<Task>, name: String) -> io::Result<Task> {
+    let task = Task {
         name: name,
         is_completed: false,
-    });
-    Ok(())
+    };
+    tasks.push(task.clone());
+    Ok(task)
 }
 
 fn list_tasks(tasks: &Vec<Task>) {
@@ -61,8 +63,104 @@ fn list_tasks(tasks: &Vec<Task>) {
 3. Exit");
 }
 
-fn complete_task(tasks: &mut Vec<Task>, level: i32) -> io::Result<()> {
+fn complete_task(tasks: &mut Vec<Task>, level: i32) -> io::Result<Task> {
     tasks[level as usize].is_completed = true;
-    Ok(())
+    Ok(tasks[level as usize].clone())
 }
 
+extern crate insta;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::{assert_json_snapshot, assert_compact_json_snapshot};
+
+    #[test]
+    fn test_json_task_struct() {
+        let task: Task = Task {
+            name: "name".to_string(),
+            is_completed: false,
+        };
+        assert_json_snapshot!(task, @r###"{
+  "name": "name",
+  "is_completed": false
+}"###);
+        assert_compact_json_snapshot!(task, @r###"{"name": "name", "is_completed": false}"###);
+    }
+
+    #[test]
+    fn test_json_task_empty_struct_vec() {
+        let tasks: Vec<Task> = vec![];
+        assert_json_snapshot!(tasks, @r###"[]"###);
+    }
+
+    #[test]
+    fn test_json_task_struct_vec() {
+        let tasks: Vec<Task> = vec![Task {
+            name: "name".to_string(),
+            is_completed: false,
+        }];
+        assert_json_snapshot!(tasks, @r###"[
+  {
+    "name": "name",
+    "is_completed": false
+  }
+]"###);
+        assert_compact_json_snapshot!(tasks, @r###"[{"name": "name", "is_completed": false}]"###);
+    }
+
+    #[test]
+    fn test_json_add_task_struct_vec() {
+        let mut tasks: Vec<Task> = vec![];
+
+        let task: Task = add_task(&mut tasks, "name".to_string()).unwrap();
+        assert_json_snapshot!(task, @r###"{
+  "name": "name",
+  "is_completed": false
+}"###);
+        assert_compact_json_snapshot!(task, @r###"{"name": "name", "is_completed": false}"###);
+
+        assert_json_snapshot!(tasks, @r###"[
+  {
+    "name": "name",
+    "is_completed": false
+  }
+]"###);
+        assert_compact_json_snapshot!(tasks, @r###"[{"name": "name", "is_completed": false}]"###);
+    }
+
+    #[test]
+    fn test_json_complete_task_struct_vec() {
+        let mut tasks: Vec<Task> = vec![];
+
+        let task: Task = add_task(&mut tasks, "name".to_string()).unwrap();
+        assert_json_snapshot!(task, @r###"{
+  "name": "name",
+  "is_completed": false
+}"###);
+        assert_compact_json_snapshot!(task, @r###"{"name": "name", "is_completed": false}"###);
+
+        assert_json_snapshot!(tasks, @r###"[
+  {
+    "name": "name",
+    "is_completed": false
+  }
+]"###);
+        assert_compact_json_snapshot!(tasks, @r###"[{"name": "name", "is_completed": false}]"###);
+
+        let task: Task = complete_task(&mut tasks, 0).unwrap();
+        assert_json_snapshot!(task, @r###"{
+  "name": "name",
+  "is_completed": true
+}"###);
+        assert_compact_json_snapshot!(task, @r###"{"name": "name", "is_completed": true}"###);
+
+        assert_json_snapshot!(tasks, @r###"[
+  {
+    "name": "name",
+    "is_completed": true
+  }
+]"###);
+        assert_compact_json_snapshot!(tasks, @r###"[{"name": "name", "is_completed": true}]"###);
+    }
+}

a70a06718edabe925e6da38ace445ada35a6edd4
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-01-09 11:39:43 +0700

    feat: create cli app

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::env;
 use std::io::{self, BufRead};
-use std::path::Path;
 
 struct Task {
-    name: String,
-    is_completed: bool,
+    pub name: String,
+    pub is_completed: bool,
 }
 
 fn readline() -> String {
@@ -19,25 +17,52 @@ fn readline() -> String {
 
 fn main() -> io::Result<()> {
     let mut tasks: Vec<Task> = vec![];
-    while true {
-        println!("{}", readline());
-        break;
+    loop {
+        list_tasks(&tasks);
+
+        let option = readline();
+        _ = match option.as_str() {
+            "1" => {
+                println!("Enter new task name: ");
+                let name = readline();
+                add_task(&mut tasks, name);
+            },
+            "2" => {
+                println!("Enter task to complete: ");
+                let level: i32 = readline().parse::<i32>().unwrap();
+                complete_task(&mut tasks, level);
+            },
+            _ => break,
+        };
     }
     Ok(())
 }
 
-fn add_task(tasks: &Vec<Task>) -> io::Result<()> {
-    // TODO: Add logic
+fn add_task(tasks: &mut Vec<Task>, name: String) -> io::Result<()> {
+    tasks.push(Task {
+        name: name,
+        is_completed: false,
+    });
     Ok(())
 }
 
-fn list_tasks(tasks: &Vec<Task>) -> io::Result<()> {
-    // TODO: List logic
-    Ok(())
+fn list_tasks(tasks: &Vec<Task>) {
+    for _ in 0..50 {
+        println!("\n");
+    }
+    println!("Tasks List: ");
+    for task in tasks {
+        println!("Name: {}", task.name);
+        println!("Is Completed: {}", task.is_completed);
+    }
+    println!("Choose the following options:
+1. Add tasks
+2. Complete tasks
+3. Exit");
 }
 
-fn complete_task(tasks: &Vec<Task>) -> io::Result<()> {
-    // TODO: Complete logic
+fn complete_task(tasks: &mut Vec<Task>, level: i32) -> io::Result<()> {
+    tasks[level as usize].is_completed = true;
     Ok(())
 }
 
